229d96c8343f92e14afadac090b8c130121a861d
Jack Tierney <jack.tierney@student.ucc.ie>
2022-06-29 13:46:51 +0100

    simplified adapter search

--- scripts/get_adapters.py
+++ scripts/get_adapters.py
@@ -32,12 +32,12 @@ def check_adapter(adapter, fastq_path, number_of_reads=2000000, verbose=False):
     if fastq_path.split('.')[-1] == 'gz':
         adapter_count_raw = subprocess.check_output(
             f"gzip -cd {fastq_path} | head -{number_of_reads} | sed -n '2~4p' | agrep -c1 \"{adapter}\"",
-            shell=True,
+            shell=True, check=True, capture_output=True,
         )
     elif fastq_path.split('.')[-1] == 'fastq' or fastq_path.split('.')[-1] == 'fq':
         adapter_count_raw = subprocess.check_output(
             f"head -{number_of_reads} {fastq_path} | sed -n '2~4p' | agrep -c1 \"{adapter}\"",
-            shell=True,
+            shell=True, check=True, capture_output=True,
         )
 
     adapter_count = float(adapter_count_raw.decode('utf-8').strip('\n'))
