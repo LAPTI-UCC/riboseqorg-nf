b1fe803a038829c950b64bc4be92893eabdb69a7
Jack Tierney <jack.tierney@student.ucc.ie>
2022-07-22 10:13:27 +0100

    get sraRunInfo csvs as channel

--- scripts/ffq_fetch_fastq.py
+++ scripts/ffq_fetch_fastq.py
@@ -23,9 +23,8 @@ def run_get_fastq(runInfo_path, outdir):
 
         # if not os.path.exists(outdir):
         #     os.makedirs(outdir)
-        raise Exception(ffq_metadata_dict['url'])
-        subprocess.run(f"wget {ffq_metadata_dict['url']} -P {outdir} ", check=True, capture_output=True, shell=True)
-
+        a = subprocess.run(f"wget {ffq_metadata_dict['url']} -P {outdir} ", check=True, capture_output=True, shell=True)
+        raise Exception(a)
 
 
 if __name__ == '__main__':
