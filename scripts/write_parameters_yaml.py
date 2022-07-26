50d4ef9a6e5c8411386ce519f5ea0e817fa6eaee
Jack Tierney <jack.tierney@student.ucc.ie>
2022-07-26 11:49:28 +0100

    added forward slash

--- scripts/write_parameters_yaml.py
+++ scripts/write_parameters_yaml.py
@@ -82,7 +82,7 @@ def write_paramters_yaml(organism, adapter_report_path, yaml_outpath, skip_gwips
 
     parameter_dict['adapter_fasta'] = adapter_report_path
     parameter_dict['study_dir'] = '/'.join(adapter_report_path.split('/')[:-2])
-    parameter_dict['fastq_files'] = '/'.join(adapter_report_path.split('/')[:-1]) + '*.fastq.gz'
+    parameter_dict['fastq_files'] = '/'.join(adapter_report_path.split('/')[:-1]) + '/*.fastq.gz'
 
     for i in zip(parameter_order[1:-2], reference_details[1:]):
         parameter_dict[i[0]] = i[1]
