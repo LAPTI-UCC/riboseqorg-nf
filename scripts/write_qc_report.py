bb3ebf21fbd6707d930605e715f148335551effa
Jack Tierney <jack.tierney@student.ucc.ie>
2022-08-03 15:17:49 +0100

    tweaks to qc report updates

--- scripts/write_qc_report.py
+++ scripts/write_qc_report.py
@@ -107,6 +107,29 @@ def get_average_readlengths(read_length_dict):
     return round(numerator/number_of_reads, 2)
 
 
+def process_transcript_reads(transcript, cds_start, cds_stop, sequence_length, transcript_reads):
+    '''
+    get gene body distribution and profile for this transcript
+    '''
+    gene_body = {'fiveprime':0, 'cds':0, 'threeprime':0}
+
+    metagene = {'fiveprime':{i:0 for i in range(-300, 301)}, 
+                    'threeprime':{i:0 for i in range(-300, 301)}}
+
+    for readlength in transcript_reads:
+        for position in transcript_reads[readlength]:
+            if position in range(0, cds_start):
+                gene_body['fiveprime'] += transcript_reads[readlength][position]
+
+            elif position in range(cds_start, cds_stop):
+                gene_body['cds'] += transcript_reads[readlength][position]
+
+            elif position in range(cds_stop, sequence_length):
+                gene_body['threeprime'] += transcript_reads[readlength][position]
+
+
+    print(gene_body)
+
 def generate_profile(sqlite_dict, organism_sqlite):
     '''
     create a whole gene metagene profile from the given sqlite dict
@@ -124,7 +147,9 @@ def generate_profile(sqlite_dict, organism_sqlite):
         transcript, cds_start, cds_stop, sequence = row
         transcript_reads = sqlite_dict[transcript]["unambig"]
 
-        print(transcript_reads)
+        if cds_start != None and cds_stop !=None:
+            process_transcript_reads(transcript, cds_start, cds_stop, len(sequence) transcript_reads)
+
         break
 
 
