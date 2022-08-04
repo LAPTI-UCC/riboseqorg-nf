3f6582c4796107b2629e0c711428d0d34e51de5d
Jack Tierney <jack.tierney@student.ucc.ie>
2022-08-04 09:04:43 +0100

    tweaks to qc report updates

--- scripts/write_qc_report.py
+++ scripts/write_qc_report.py
@@ -197,10 +197,14 @@ def calculate_metagene_summary_score(metagene):
 
     for prime in metagene:
         print()
+        print('fiveprime', sum([i for i in metagene[prime] if i < 0])/300)
+        print('threeprime', sum([i for i in metagene[prime] if i >= 0])/300)
+
+
         positions = sorted(metagene[prime].keys())
         for window in sliding_window(positions):
             metagene_window = [metagene[prime][i] for i in window]
-            print(sum(metagene_window)/len(metagene_window))
+            # print(sum(metagene_window)/len(metagene_window))
 
 
 
