5029b3dcbecc217c23b41bfd90b75003b505d2ff
Gionmattia Carancini <gionmattia@gmail.com>
2022-08-15 17:06:17 +0100

    added comment to report_model.py

--- scripts/report_model.py
+++ scripts/report_model.py
@@ -1,3 +1,5 @@
 ${task_id} = {"status" : "${status}", "info" : {"script": """${script}""", 
 "exit_status": "${exit}",  "workdir": "${workdir}"}}
-    
+
+# nextflow log <run-name> -t scripts/exec_report.py > test.txt
+# it's important to keep the model as a .py script and the output as a txt for easiness for interpretation
