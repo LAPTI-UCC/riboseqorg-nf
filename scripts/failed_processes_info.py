22bcb4ca7a26d4851e60437545a4df5a5d8ccd94
Gionmattia <gionmattia@gmail.com>
2022-08-16 12:27:37 +0100

    failed_processes_info.py
    failed_processes_info.py has been added. It returns a list of GSE not downloaded by the pipeline. The previous report_model.py has been removed, because not needed anymore

---
+++ scripts/failed_processes_info.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import argparse
+
+def get_GSE_not_downloaded(df):
+    '''
+    Returns the list of GSEs the process GET_GSE_REPORT was not able to download
+    '''
+    failed_df = df.loc[df['status'] == "FAILED"]
+    df2 = failed_df[failed_df['name'].str.contains('REPORT')]
+
+    not_downloaded_list = []
+    for n in df2.index:
+        raw_GSE = df2.at[n,"raw_GSE"]
+        GSE = raw_GSE[raw_GSE.index("GSE"):raw_GSE.index("_family")]
+        not_downloaded_list.append(GSE)
+
+    return not_downloaded_list
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Retrieves the GSEs that have not been downloaded correctly")
+    parser.add_argument("path_to_run_report", type = str, help = "Path to the location of the run report, a csv file")
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.path_to_run_report, header = None, names = ["name", "status", "exit", "workdir", "raw_GSE"])
+    get_GSE_not_downloaded(df)
+
--- scripts/report_model.py
+++
@@ -1,3 +0,0 @@
-${task_id} = {"status" : "${status}", "info" : {"script": """${script}""", 
-"exit_status": "${exit}",  "workdir": "${workdir}"}}
-
