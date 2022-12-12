26f4a257084735dae14719286441216731ad6262
Jack Tierney <jack.tierney@student.ucc.ie>
2022-12-12 18:03:26 +0000

    check readcoiunt

--- scripts/bam_to_bed.py
+++ scripts/bam_to_bed.py
@@ -45,10 +45,9 @@ def process_chromosome(all_reads, offset):
 		protect_nts.sort()
 
 		# support for collapsed reads
-		print(int(read.qname.split("_x")[1]))
-
 		if "_x" in read.qname:
 			read_count = int(read.qname.split("_x")[1])
+			print(f"Readcount: {read_count}")
 		else:
 			read_count = 1
 
